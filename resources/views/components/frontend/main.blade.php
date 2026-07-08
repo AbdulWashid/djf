0ca3a83b1c7d7fde188d5c48d1e8e322d5443108
AbdulWashid <abdul.washid2004@gmail.com>
2026-07-08 18:55:13 +0530

    google tag manager add

--- resources/views/components/frontend/main.blade.php
+++ resources/views/components/frontend/main.blade.php
@@ -380,6 +380,14 @@
         });
     </script>
     @stack('schema')
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-78EEVRHQBJ"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', 'G-78EEVRHQBJ');
+    </script>
 </head>
 
 <body>
